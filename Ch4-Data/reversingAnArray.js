da2184aafa4a568dfa348da24771067be64f9d1e
Yinyin W <xiaodian.wang@gmail.com>
2015-03-23 20:32:16 -0700

    Completed reversingAnArray.js and sumOfRange.js

---
+++ Ch4-Data/reversingAnArray.js
@@ -0,0 +1,46 @@
+/*REVERSING AN ARRAY:
+Write a function reverseArray(), that takes an array as an argument
+and produces a new array that has the same elements in the inverse order.
+This may not use the standard reverse() method.
+*/
+function reverseArray(arr) {
+	var reverseArr = [];
+	if (arguments.length >= 1) {
+		for (var i=0; i<arr.length; i++) {
+			reverseArr.unshift(arr[i]);
+		}	
+	}
+	return reverseArr;
+}
+
+/*
+Write a function reverseArrayInPlace(), that does what the reverseArray()
+method does: it modifies the array given as argument in order to reverse
+its elements.
+*/
+function reverseArrayInPlace(arr) {
+	if (arr && arr.length > 0) {
+		var startInd = 0;
+		var endInd = arr.length - 1;
+		while (startInd < endInd) {
+			var el1 = arr[startInd];
+			var el2 = arr[endInd];
+			arr[startInd] = el2;
+			arr[endInd] = el1;
+			startInd++;
+			endInd--;
+		}
+		return arr;
+	} else {
+		return [];
+	}
+}
+
+/*
+Calling reverseArrayInPlace() on the following arguments below, I would
+expect the following results:
+*/
+console.log(reverseArrayInPlace()); //should print "[]"
+console.log(reverseArrayInPlace([]); //should print "[]"
+console.log(reverseArrayInPlace([1])); //should print "[1]"
+console.log(reverseArrayInPlace(["a", 1.23, 4, 5])) //should print "[5, 4, 1.23, "a"]"
---
+++ Ch4-Data/sumOfRange.js
@@ -0,0 +1,88 @@
+/*THE SUM OF A RANGE:
+Write a range() function that takes two arguments, start and end, and returns an array
+containing all the numbers from start up to (and including) end.
+*/
+function range(start, end) {
+	var minNum = Math.min(start, end);
+	var maxNum = Math.max(start, end);
+	var rangeArr = [];
+	for (minNum; minNum <= maxNum; minNum++) {
+		if (start > end) {
+			rangeArr.unshift(minNum);
+		} else {
+			rangeArr.push(minNum);
+		}
+	}
+	return rangeArr;
+}
+/*Write a sum() function that takes an array of numbers and returns the sum of these
+numbers. Run the program console.log(sum(range(1,10))) and see whether it does indeed
+return 55.
+*/
+function sum(numArr) {
+	if (arguments.length == 0) {
+		return 0;
+	} else {
+		var total = 0;
+		for(var i=0; i<numArr.length; i++) {
+			total+=numArr[i];
+		}
+		return total;
+	}
+}
+
+function sum(numArr) {
+	if (arguments.length == 0) {
+		return 0;
+	} else {
+		var total = 0;
+		for (var num in numArr) {
+			total += parseInt(num);
+		}
+		return total;
+	}
+}
+//Calling the following function below, I would expect it to print 55.
+console.log(sum(range(1,10)));
+
+/*Modify your range() function to take an optional third argument that indicates the
+"step" value used to build up the array. If no step is given, the array elements go 
+up by increments of one, corresponding to the old behavior.
+*/
+//num1 < num2, step is (+) -> start = num1, end=num2
+//num1 < num2, step is (-) -> start = num2, end=num1
+//num1 > num2, step is (+) -> start = num2, end=num1
+//num1 > num2, step is (-) -> start = num1, end=num2
+function range(start, end, step) {
+	var minNum = Math.min(start, end);
+	var maxNum = Math.max(start, end);
+	var allInRange = [];
+	//build an array containing all integers within [start,end], with
+	//increment of 1
+	for(minNum; minNum <= maxNum; minNum++) {
+		allInRange.push(minNum);
+	}
+	if (arguments.length < 3) {
+		if (start > end) {
+			step = -1;
+		} else {
+			step = 1;
+		}
+	}
+	var startIndex = allInRange.indexOf(start);
+	var rangeArr = [];
+	while (startIndex > -1 && startIndex < allInRange.length) {
+		rangeArr.push(allInRange[startIndex]);
+		startIndex+=step;
+	}
+	return rangeArr;
+}
+
+//The function call range(1,10,2) should return [1, 3, 5, 7, 9]
+//It should also work with negative step values so that range(5,2,-1) produces [5,4,3,2]
+/*Note: This exercise seems to be assuming that when the range() function is called,
+the sign of the step increment corresponds to the order of your start
+and end numbers. 
+*/
+console.log(range(1,10,2));
+console.log(range(5,2,-1));
